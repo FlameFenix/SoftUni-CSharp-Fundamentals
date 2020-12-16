7fbb1caeee5ac87ed2b01fce38a3c771a77002dc
Yordan Dimitrov <68155186+FlameFenix@users.noreply.github.com>
2020-12-16 22:00:14 +0200

    exercise

---
+++ Regular Expressions - Exercise/Furniture/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace Furniture
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+
+            string pattern = @">>(?<object>[A-Za-z]+)<<(?<price>\d+.?\d*)!(?<quantity>\d+)";
+
+            List<string> boughtObjects = new List<string>();
+
+            double moneySpend = 0;
+
+            Regex regex = new Regex(pattern);
+
+            Console.WriteLine("Bought furniture:");
+
+            while (command != "Purchase")
+            {
+
+                Match matches = regex.Match(command);
+
+                if (matches.Success)
+                {
+                    string item = matches.Groups["object"].Value;
+                    double price = double.Parse(matches.Groups["price"].Value);
+                    int quantity = int.Parse(matches.Groups["quantity"].Value);
+
+                    moneySpend += quantity * price;
+                    boughtObjects.Add(item);
+                    Console.WriteLine(item);
+                }
+                command = Console.ReadLine();
+            }
+            Console.WriteLine($"Total money spend: {moneySpend:f2}");
+        }
+    }
+}
---
+++ Regular Expressions - Exercise/Race/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Race
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] participants = Console.ReadLine()
+                                           .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                                           .ToArray();
+
+
+
+            Dictionary<string, int> race = new Dictionary<string, int>();
+
+            foreach (var name in participants)
+            {
+                race.Add(name, 0);
+            }
+            string command = Console.ReadLine();
+
+            while (command != "end of race")
+            {
+                string namePattern = @"[\W\d]";
+                string distancePattern = @"[\WA-Za-z]";
+
+                string name = Regex.Replace(command, namePattern, "");
+                string distance = Regex.Replace(command, distancePattern, "");
+                int kilometers = 0;
+                foreach (var digit in distance)
+                {
+                    int currentNumber = int.Parse(digit.ToString());
+                    kilometers += currentNumber;
+                }
+
+                if (race.ContainsKey(name))
+                {
+                    race[name] += kilometers;
+                }
+
+                command = Console.ReadLine();
+            }
+
+            int counter = 0;
+
+            foreach (var item in race.OrderByDescending(x => x.Value))
+            {
+                counter++;
+                string text = counter == 1 ? "st" : counter == 2 ? "nd" : "rd";
+
+                Console.WriteLine($"{counter}{text} place: {item.Key}");
+                if (counter == 3)
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
