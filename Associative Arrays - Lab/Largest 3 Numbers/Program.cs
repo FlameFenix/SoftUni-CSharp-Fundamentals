59fdf6b215999d9fa736544bc17def1b65d279f6
Yordan Dimitrov <68155186+FlameFenix@users.noreply.github.com>
2020-12-16 21:30:18 +0200

    lab

---
+++ Associative Arrays - Lab/Largest 3 Numbers/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace Largest_3_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .OrderByDescending(n => n)
+                .ToArray();
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (i < 3)
+                {
+                    Console.Write($"{numbers[i]} ");
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
