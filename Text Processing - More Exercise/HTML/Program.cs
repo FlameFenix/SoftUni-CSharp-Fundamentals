5ea78822b644023f23d8fd233c21212f0bb37131
Yordan Dimitrov <68155186+FlameFenix@users.noreply.github.com>
2021-01-01 21:26:16 +0200

    happy new year ;p

---
+++ Text Processing - More Exercise/HTML/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HTML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = string.Empty;
+
+            int counter = 0;
+
+            List<string> htmlCode = new List<string>();
+
+            while ((input = Console.ReadLine()) != "end of comments")
+            {
+                counter++;
+
+                if(counter == 1)
+                {
+                    htmlCode.Add("<h1>");
+                    htmlCode.Add(input);
+                    htmlCode.Add("</h1>");
+                }
+                else if(counter == 2)
+                {
+                    htmlCode.Add("<article>");
+                    htmlCode.Add(input);
+                    htmlCode.Add("</article>");
+                }
+                else
+                {
+                    htmlCode.Add("<div>");
+                    htmlCode.Add(input);
+                    htmlCode.Add("</div>");
+                }
+            }
+
+            Console.WriteLine(string.Join(Environment.NewLine, htmlCode));
+        }
+    }
+}
--- Text Processing - More Exercise/Treasure Finder/Program.cs
+++ Text Processing - More Exercise/Treasure Finder/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace Treasure_Finder
 {
@@ -6,7 +7,49 @@ namespace Treasure_Finder
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] numbers = Console.ReadLine()
+                                   .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                                   .Select(int.Parse)
+                                   .ToArray();
+
+            string input = string.Empty;
+
+            while ((input = Console.ReadLine()) != "find")
+            {
+                string newInput = string.Empty;
+
+                int countKeys = 0;
+
+                for (int j = 0; j < input.Length; j++)
+                {
+                    if (countKeys >= numbers.Length)
+                    {
+                        countKeys = 0;
+                    }
+
+                    newInput += (char)(input[j] - numbers[countKeys]);
+                    countKeys++;
+                }
+                string type = string.Empty;
+                int startIndex = newInput.IndexOf('&');
+
+                for (int i = startIndex; i < newInput.Length; i++)
+                {
+                    if(newInput[i + 1] == '&')
+                    {
+                        break;
+                    }
+
+                    type += newInput[i + 1];
+                }
+
+                int indexOfCords = newInput.IndexOf('<');
+                int endIndexOfCords = newInput.IndexOf('>');
+
+                string coordinates = newInput.Substring(indexOfCords + 1, (endIndexOfCords - indexOfCords) - 1);
+
+                Console.WriteLine($"Found {type} at {coordinates}");
+            }
         }
     }
 }
